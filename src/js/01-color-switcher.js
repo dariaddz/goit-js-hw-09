f02400d01654d210c45b7a1104d97dd79e4f74d0
dariaddz <dariadz@gmail.com>
2021-11-23 12:51:00 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', startSwitcher);
+stopBtn.addEventListener('click', stopSwitcher);
+
+function onBodyColorChange() {
+  let color = null;
+  color = getRandomHexColor();
+  body.style.backgroundColor = `${color}`;
+}
+
+function startSwitcher() {
+  timerId = setInterval(onBodyColorChange, 1000);
+  startBtn.disabled = true;
+}
+
+function stopSwitcher() {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
