5bb5ef75a75c5fc02db648cae023c45915c40e81
dariaddz <dariadz@gmail.com>
2021-11-24 11:59:58 +0200

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,48 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 // ----------------------------------------------
 const timerMarkup = document.querySelector('.timer');
 const fields = document.querySelectorAll('.field');
-const values = document.querySelectorAll('.value');
 const labels = document.querySelectorAll('.label');
 const cndwnDays = document.querySelector('.value[data-days]');
 const cndwnHours = document.querySelector('.value[data-hours]');
 const cndwnMinutes = document.querySelector('.value[data-minutes]');
 const cndwnSeconds = document.querySelector('.value[data-seconds]');
-// console.log(cndwnSeconds);
 const startBtn = document.querySelector('button[data-start]');
 
+// ---создаем календарь с опцией проверки прошлой даты и notify------
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (calendar.selectedDates[0] < Date.now()) {
-      alert('Please choose a date in the future');
+    if (calendar.selectedDates[0] <= Date.now()) {
+      Notify.failure('Please choose a date in the future');
       startBtn.disabled = true;
     } else {
       startBtn.disabled = false;
     }
-    console.log(selectedDates[0]);
   },
 };
-const calendar = flatpickr(document.querySelector('input#datetime-picker'), options);
-if (calendar.selectedDates[0] < Date.now()) {
-  alert('Please choose a date in the future');
-  startBtn.disabled = true;
-} else {
-  startBtn.disabled = false;
-}
-// -----------------
 
-// берем дату в будущем
-// вычисляем разницу между будущей и текущей датой
-//делаем эти вычисления раз в секунду
-// разницу приводим к дням / часам / минутам / секундам
-// записываем приведенную дату в поля таймера
-// обновляем поля
-//---------------------------делаем класс-----------------------------
+const calendar = flatpickr(document.querySelector('input#datetime-picker'), options);
+
+//---------------------делаем класс Timer -------------------------
+// ----------------с остановкой если время пришло------------------
 class Timer {
   constructor({ onTick }) {
     this.onTick = onTick;
@@ -52,13 +40,21 @@ class Timer {
     setInterval(() => {
       const currentDate = Date.now();
       const countdown = futureDate - currentDate;
+      if (futureDate - currentDate < 1000) {
+        console.log('время вышло');
+        cndwnSeconds.textContent = '00';
+        for (let field of fields) {
+          field.style.color = '#9e2611';
+        }
+        return;
+      }
       const time = this.convertMs(countdown);
       this.onTick(time);
       startBtn.disabled = true;
     }, 1000);
   }
 
-  // -------------перевод мс в дни/часы и тд--------------------------
+  // -------------перевод мс в дни/часы и тд, с добавлением нулей--------------------------
   addLeadingZero(value) {
     return String(value).padStart(2, '0');
   }
@@ -78,6 +74,7 @@ class Timer {
   }
 }
 
+// ---------создаем таймер из класса с функцией обновления -----------
 const timer = new Timer({ onTick: updateCountdown });
 
 // ------------------------слушатель на кнопку Старт------
